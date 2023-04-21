8a1dabb27c3be4b77f3825c46e147bd6f6918f3e
Gustavo Landi <gustavo.landi@gmail.com>
2023-04-21 18:27:43 -0300

    adding input box

---
+++ src/components/InputBox/index.js
@@ -0,0 +1,58 @@
+import { View, TextInput, StyleSheet } from 'react-native';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons'; 
+import { useState } from 'react';
+
+const InputBox = () => {
+
+    const [newMessage, setNewMessage] = useState('');
+
+    const onSend = () => {
+        console.warn("Send a new message",newMessage);
+        setNewMessage("");
+      };
+
+    return (
+        <View style={styles.container}>
+            <AntDesign name='plus' size={24} color='royalblue' />
+
+            <TextInput
+                value={newMessage}
+                onChangeText={setNewMessage}
+                style={styles.input} 
+                multiline
+            />
+            <MaterialIcons onPress={onSend} style={styles.send} name='send' size={24} color='white'/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flexDirection: 'row',
+        backgroundColor: 'whitesmoke',
+        padding: 5,
+        paddingHorizontal: 10,
+        alignItems: 'center',
+    },
+    input : {
+        flex: 1,
+        backgroundColor: 'white',
+        padding: 5,
+        paddingHorizontal: 10,
+        marginHorizontal: 10,
+
+        borderRadius: 50,
+        borderColor: 'lightgray',
+        borderWidth: StyleSheet.hairlineWidth,
+
+    },
+    send: {
+        backgroundColor: 'royalblue',
+        padding: 7,
+        borderRadius: 15,
+        overflow: 'hidden',
+
+    }
+});
+
+export default InputBox;
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,18 +1,24 @@
-import { StyleSheet, View, Text, ImageBackground, FlatList } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, ImageBackground, FlatList } from 'react-native';
 import Message from '../components/Message';
 import messages from '../../assets/data/messages.json'
 import bg from '../../assets/images/BG.png'
+import InputBox from '../components/InputBox';
 
 const ChatScreen = () => {
     return (
-        <ImageBackground source={bg} style={styles.bg}>
-            <FlatList 
-                data={messages}
-                renderItem={({item}) => <Message message={item}></Message>}
-                style={styles.list}
-                inverted
-            />
-        </ImageBackground>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.bg}>
+            <ImageBackground source={bg} style={styles.bg}>
+                <FlatList 
+                    data={messages}
+                    renderItem={({item}) => <Message message={item}></Message>}
+                    style={styles.list}
+                    inverted
+                />
+                <InputBox/>
+            </ImageBackground>
+        </KeyboardAvoidingView>
     );
 };
 
