9e8e879c0fe958245c3e7c8a9814e868e19798c0
Gustavo Landi <gustavo.landi@gmail.com>
2023-05-07 13:42:29 -0300

    uploading image and viewing image

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,13 +1,49 @@
-import { View, TextInput, StyleSheet } from 'react-native';
+import { Image, View, TextInput, StyleSheet } from 'react-native';
 import { AntDesign, MaterialIcons } from '@expo/vector-icons'; 
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { API, Auth, graphqlOperation } from 'aws-amplify';
+import { API, Auth, graphqlOperation, Storage } from 'aws-amplify';
 import { createMessage, updateChatRoom } from '../../graphql/mutations';
+import * as ImagePicker from 'expo-image-picker';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 const InputBox = ({ chatroom }) => {
 
     const [text, setText] = useState('');
+    const [image,setImage] = useState(null);
+
+    const uploadFile = async (fileUri) => {
+        try {
+          const response = await fetch(fileUri);
+          const blob = await response.blob();
+          const key = `${uuidv4()}.png`;
+          await Storage.put(key, blob, {
+            contentType: "image/png", // contentType is optional
+          });
+          return key;
+        //   await Storage.put(blob._data.name, blob, {
+ 
+        //   });
+        //   return blob._data.name;
+        } catch (err) {
+          console.log("Error uploading file:", err);
+        }
+      };
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+      };
 
     const onSend = async () => {
         const authUser = await Auth.currentAuthenticatedUser();
@@ -17,6 +53,11 @@ const InputBox = ({ chatroom }) => {
             text: text,
             userID: authUser.attributes.sub
         };
+
+        if (image) {
+            newMessageInput.images = [await uploadFile(image)];
+            setImage(null);
+          }
 
         const newMessageData = await API.graphql(graphqlOperation(createMessage, {input : newMessageInput}));
 
@@ -30,17 +71,35 @@ const InputBox = ({ chatroom }) => {
       };
 
     return (
-        <SafeAreaView edges={["bottom"]} style={styles.container}>
-            <AntDesign name='plus' size={24} color='royalblue' />
+        <>
+      {image && (
+        <View style={styles.attachmentsContainer}>
+          <Image
+            source={{ uri: image }}
+            style={styles.selectedImage}
+            resizeMode="contain"
+          />
+          <MaterialIcons
+            name="highlight-remove"
+            onPress={() => setImage(null)}
+            size={20}
+            color="gray"
+            style={styles.removeSelectedImage}
+          />
+        </View>
+      )}
+            <SafeAreaView edges={["bottom"]} style={styles.container}>
+                <AntDesign onPress={pickImage}name='plus' size={24} color='royalblue' />
 
-            <TextInput
-                value={text}
-                onChangeText={setText}
-                style={styles.input} 
-                multiline
-            />
-            <MaterialIcons onPress={onSend} style={styles.send} name='send' size={24} color='white'/>
-        </SafeAreaView>
+                <TextInput
+                    value={text}
+                    onChangeText={setText}
+                    style={styles.input} 
+                    multiline
+                />
+                <MaterialIcons onPress={onSend} style={styles.send} name='send' size={24} color='white'/>
+            </SafeAreaView>
+        </>
     );
 }
 
@@ -70,7 +129,22 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         overflow: 'hidden',
 
-    }
+    },
+    attachmentsContainer: {
+	    alignItems: "flex-end",
+	  },
+	  selectedImage: {
+	    height: 100,
+	    width: 200,
+	    margin: 5,
+	  },
+	  removeSelectedImage: {
+	    position: "absolute",
+	    right: 10,
+	    backgroundColor: "white",
+	    borderRadius: 10,
+	    overflow: "hidden",
+	  }
 });
 
 export default InputBox;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,8 +1,10 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, Pressable } from 'react-native';
 import dayjs from 'dayjs';
 import relativeTime from "dayjs/plugin/relativeTime"
-import { Auth } from 'aws-amplify';
-import { useState } from 'react';
+import { Auth, Storage } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+import { S3Image } from 'aws-amplify-react-native/dist/Storage';
+import ImageView from 'react-native-image-viewing';
 
 dayjs.extend(relativeTime);
 
@@ -10,13 +12,32 @@ dayjs.extend(relativeTime);
 const Message = ({message}) => {
 
     const [isAuthUser,setIsAuthUser] = useState(false);
+    const [imageViewerVisible,setImageViewerVisible] = useState(false);
+    const [imageSources,setImageSourcers] = useState([]);
 
-    const isMyMessage = async () => {
-        const authUser = await Auth.currentAuthenticatedUser();
-        setIsAuthUser(message.userID === authUser.attributes.sub);
-    };
+    useEffect(() => {
+        const isMyMessage = async () => {
+            const authUser = await Auth.currentAuthenticatedUser();
+            setIsAuthUser(message.userID === authUser.attributes.sub);
+        };
+    
+        isMyMessage();
+    }, []);
 
-    isMyMessage();
+    useEffect(() => {
+        const downloadImages = async () => {
+            if (message.images?.length > 0) {
+                if (message.images[0] != null) {
+                    const uri = await Storage.get(message.images[0]);
+                    setImageSourcers([{ uri }])
+                }
+            }
+        }
+
+        downloadImages();
+    }, [message.images]);
+
+    console.log(imageSources);
 
     return (
         <View style={[styles.container,
@@ -25,6 +46,19 @@ const Message = ({message}) => {
             alignSelf: isAuthUser ? 'flex-end' : 'flex-start'
         }
         ]}>
+        {imageSources[0] != null && (
+            <>
+                <Pressable onPress={() => setImageViewerVisible(true)}>
+                    <Image source={imageSources[0]} style={styles.image}/>
+                </Pressable>
+                <ImageView
+                    images={imageSources} 
+                    imageIndex={0} 
+                    visible={imageViewerVisible} 
+                    onRequestClose={() => setImageViewerVisible(false)}
+                />
+            </>
+        )}
         <Text>{message.text}</Text>
         <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
     </View>
@@ -49,7 +83,14 @@ const styles = StyleSheet.create({
     time: {
         color: 'gray',
         alignSelf: 'flex-end'
-    }
+    },
+    image: {
+        width: 200,
+        height: 100,
+        borderColor: "white",
+        borderWidth: 2,
+        borderRadius: 5,
+      },
 });
 
 export default Message;
